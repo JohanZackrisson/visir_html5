cb1e82a0331c7dc9f82500517985795b6b054d90
Johan Zackrisson <zeta@dev.openlabs.bth.se>
2012-11-20 11:04:45 +0100

    Add base64 decoder usable for deserializing graph data

--- utils.js
+++ utils.js
@@ -53,3 +53,36 @@ visir.LightNum = function(strnum, digit) {
 	
 	return out;
 }
+
+function base64_decode(data) {
+  var b64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+  var o1, o2, o3, h1, h2, h3, h4, bits, i = 0,
+    ac = 0,
+    dec = "",
+    tmp_arr = [];
+
+  if (!data) {
+    return data;
+  }
+
+	data = data.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+
+  do {
+    h1 = b64.indexOf(data.charAt(i++));
+    h2 = b64.indexOf(data.charAt(i++));
+    h3 = b64.indexOf(data.charAt(i++));
+    h4 = b64.indexOf(data.charAt(i++));
+
+    bits = h1 << 18 | h2 << 12 | h3 << 6 | h4;
+
+    o1 = bits >> 16 & 0xff;
+    o2 = bits >> 8 & 0xff;
+    o3 = bits & 0xff;
+
+		tmp_arr[ac++] = o1;
+		if (h3 != 64) tmp_arr[ac++] = o2;
+		if (h4 != 64) tmp_arr[ac++] = o3;
+  } while (i < data.length);
+
+  return tmp_arr;
+}
