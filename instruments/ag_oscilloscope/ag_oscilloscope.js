fac90fb12613a4bcfb8588743d73113d9f098a9e
JohanZackrisson <johanz@gmail.com>
2012-11-30 14:10:33 +0100

    Add run/stop/single measurement buttons and start fiddling with an external interface for doing measurements. Can't decide on what kind of interface it should be..

--- instruments/ag_oscilloscope/ag_oscilloscope.js
+++ instruments/ag_oscilloscope/ag_oscilloscope.js
@@ -2,9 +2,15 @@
 
 var visir = visir || {};
 
-visir.AgilentOscilloscope = function(id, elem)
+visir.AgilentOscilloscope = function(id, elem, props)
 {
 	visir.AgilentOscilloscope.parent.constructor.apply(this, arguments)
+	
+	var options = $.extend({
+		MeasureCalling: function() {}
+		,CheckToContinueCalling: function() { return false; }
+	}, props || {});
+	this._options = options;
 	
 	this._voltages = [5, 2, 1, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01, 0.005, 0.002, 0.001];
 	this._voltIdx = [2,2];
@@ -16,6 +22,7 @@ visir.AgilentOscilloscope = function(id, elem)
 	this._triggerModesDisplay = ["Auto Level", "Auto", "Normal"];
 	this._triggerModesLight = ["Level", "Auto", "Trig'd"];
 	this._triggerModeIdx = 0;
+	this._triggerLevelUnclamped = 0.0;
 	
 	var me = this;
 	this._$elem = elem;
@@ -30,6 +37,9 @@ visir.AgilentOscilloscope = function(id, elem)
 	
 	this._activeIndicator = null;
 	this._menuTitleTimer = null;
+	
+	//this._isMeasuring = false;
+	this._isMeasuringContinuous = false;
 		
 	var imgbase = "instruments/ag_oscilloscope/images";
 		
@@ -76,12 +86,31 @@ visir.AgilentOscilloscope = function(id, elem)
 			<img class="up active" src="%img%/osc_trig_on_up_edge.png" alt="edge button" /><img class="down" src="%img%/osc_trig_on_down_edge.png" alt="edge button" />\
 		</div>\
 	</div>\
+	<div class="button multibutton single">\
+		<div class="state dark visible">\
+			<img class="up active" src="%img%/single_grey_up.png" alt="single button" /><img class="down" src="%img%/single_grey_down.png" alt="single button" />\
+		</div>\
+		<div class="state light">\
+			<img class="up active" src="%img%/single_yellow_up.png" alt="single button" /><img class="down" src="%img%/single_yellow_down.png" alt="single button" />\
+		</div>\
+	</div>\
+	<div class="button multibutton runstop">\
+		<div class="state dark visible">\
+			<img class="up active" src="%img%/runstop_grey_up.png" alt="run/stop button" /><img class="down" src="%img%/runstop_grey_down.png" alt="run/stop button" />\
+		</div>\
+		<div class="state green">\
+			<img class="up active" src="%img%/runstop_green_up.png" alt="run/stop button" /><img class="down" src="%img%/runstop_green_down.png" alt="run/stop button" />\
+		</div>\
+		<div class="state red">\
+			<img class="up active" src="%img%/runstop_red_up.png" alt="run/stop button" /><img class="down" src="%img%/runstop_red_down.png" alt="run/stop button" />\
+		</div>\
+	</div>\
 	<div class="display">\
 		<div class="background">\
-			<div class="channel ch1"><span class="channelname">1</span><span class="lighttext voltage_ch1">1.00V/</span></div>\
-			<div class="channel ch2"><span class="channelname">2</span><span class="lighttext voltage_ch2">1.00V/</span></div>\
+			<div class="channel ch1"><span class="channelname">1</span><span class="lighttext voltage_ch1"><span class="value">1.00</span><div class="unitstring"></div>/</span></div>\
+			<div class="channel ch2"><span class="channelname">2</span><span class="lighttext voltage_ch2"><span class="value">1.00</span><div class="unitstring"></div>/</span></div>\
 			<div class="timedelay"><img class="arrow" src="%img%/delay_arrow.png" alt="delay arrow" /><span class="lighttext">0.00s</span></div>\
-			<div class="timescale"><span class="lighttext timediv">500us</span></div>\
+			<div class="timescale"><span class="lighttext timediv"><span class="value">500us</span><div class="unitstring"></div>/</span></div>\
 			<div class="trigtype"><span class="lighttext triggermode">Level</span></div>\
 			<div class="triggerslope">\
 				<img class="flank positive selected" src="%img%/osc_trig_edge_up_small.png" alt="trigger flank" /><img class="flank negative" src="%img%/osc_trig_edge_down_small.png" alt="trigger flank" />\
@@ -89,7 +118,7 @@ visir.AgilentOscilloscope = function(id, elem)
 			<div class="triggersource">\
 				<span class="channelname">1</span>\
 			</div>\
-			<div class="triglevel"><span class="lighttext">0.00V</span></div>\
+			<div class="triglevel"><span class="lighttext"><span class="value">0.00</span><div class="unitstring"></div></span></div>\
 			<div class="vertical">\
 				<div class="group offset_group_ch1 visible">\
 					<div class="ch_offset normal visible"><span class="offsetchannel">1</span><img class="offsetarrow" src="%img%/offset_arrow.png" alt="offset arrow" /></div>\
@@ -100,6 +129,11 @@ visir.AgilentOscilloscope = function(id, elem)
 					<div class="ch_offset normal visible"><span class="offsetchannel">2</span><img class="offsetarrow" src="%img%/offset_arrow.png" alt="offset arrow" /></div>\
 					<div class="ch_offset overflow_up"><span class="offsetchannel overflow_up">2</span><img class="offsetarrow" src="%img%/offset_arrow_up.png" alt="offset arrow" /></div>\
 					<div class="ch_offset overflow_down"><span class="offsetchannel overflow_down">2</span><img class="offsetarrow" src="%img%/offset_arrow_down.png" alt="offset arrow" /></div>\
+				</div>\
+				<div class="group trigger_group visible">\
+					<div class="ch_offset normal visible"><img class="offsetarrow" src="%img%/osc_trig_marker_norm.png" alt="trigger arrow" /></div>\
+					<div class="ch_offset overflow_up"><img class="offsetarrow" src="%img%/osc_trig_marker_up.png" alt="trigger arrow" /></div>\
+					<div class="ch_offset overflow_down"><img class="offsetarrow" src="%img%/osc_trig_marker_down.png" alt="trigger arrow" /></div>\
 				</div>\
 			</div>\
 			<div class="graph">\
@@ -190,6 +224,10 @@ visir.AgilentOscilloscope = function(id, elem)
 	tpl = tpl.replace(/%img%/g, imgbase);
 		
 	elem.append(tpl);
+	
+	var unitstring_tpl = '<div class="large strings visible">V</div><div class="small strings"><div class="top">m</div><div class="bottom">v</div></div>'
+	elem.find(".unitstring").append(unitstring_tpl);
+	elem.find(".timescale .bottom").text("s");
 
 	var prev = 0;
 	
@@ -221,7 +259,7 @@ visir.AgilentOscilloscope = function(id, elem)
 	elem.find(".offset_ch1").turnable({turn: newHandleFunc(function() { me._StepDisplayOffset(0, true); }, function() { me._StepDisplayOffset(0, false); }) });
 	elem.find(".offset_ch2").turnable({turn: newHandleFunc(function() { me._StepDisplayOffset(1, true); }, function() { me._StepDisplayOffset(1, false); }) });
 	
-	elem.find(".offset_trg").turnable({turn: newHandleFunc(function() { trace("up");}, function() {trace("down");}) });
+	elem.find(".offset_trg").turnable({turn: newHandleFunc(function() { me._StepTriggerLevel(true); }, function() { me._StepTriggerLevel(false); }) });
 	elem.find(".horz").turnable({turn: newHandleFunc(function() { me._SetTimedivIdx(me._timeIdx+1); }, function() { me._SetTimedivIdx(me._timeIdx-1); }) });
 	elem.find(".vert_ch1").turnable({turn: newHandleFunc(function() { me._SetVoltIdx(0, me._voltIdx[0]+1); }, function() { me._SetVoltIdx(0, me._voltIdx[0]-1);}) });
 	elem.find(".vert_ch2").turnable({turn: newHandleFunc(function() { me._SetVoltIdx(1, me._voltIdx[1]+1); }, function() { me._SetVoltIdx(1, me._voltIdx[1]-1);}) });
@@ -245,6 +283,15 @@ visir.AgilentOscilloscope = function(id, elem)
 	
 	elem.find(".button.modecoupling").click( function() {
 		me._ShowMenu("menu_modecoupling");
+	});
+	
+	elem.find(".button.single").click( function() {
+		trace("single");
+		me._MakeMeasurement("single");
+	});
+	elem.find(".button.runstop").click( function() {
+		trace("runstop");
+		me._MakeMeasurement("runstop");
 	});
 	
 	elem.find(".display_button_1").click( function() { me._DisplayButtonClicked(1); })
@@ -368,8 +415,11 @@ visir.AgilentOscilloscope.prototype._SetVoltIdx = function(ch, idx)
 	
 	var indicatorName = (ch == 0 ? ".voltage_ch1" : ".voltage_ch2");
 	var $indicator = this._$elem.find(indicatorName);
-	$indicator.text(this._FormatValue(this._voltages[this._voltIdx[ch]]) + "V");
+	/*$indicator.text(this._FormatValue(this._voltages[this._voltIdx[ch]]) + "V");
+	*/
+	
 	this._LightIndicator($indicator);
+	this._UpdateTriggerLevel();
 	this._UpdateDisplay();
 }
 
@@ -381,7 +431,7 @@ visir.AgilentOscilloscope.prototype._SetTimedivIdx = function(idx)
 	//trace("timediv idx: " + idx);
 	this._sampleRate = 1.0 / this._timedivs[this._timeIdx]; // sets value for serialization
 	var $indicator = this._$elem.find(".timediv");
-	$indicator.text(this._FormatValue(this._timedivs[this._timeIdx]) + "s");
+	//$indicator.text(this._FormatValue(this._timedivs[this._timeIdx]) + "s");
 	this._LightIndicator($indicator);
 	this._UpdateDisplay();
 }
@@ -417,8 +467,85 @@ visir.AgilentOscilloscope.prototype._SetDisplayOffset = function(ch, offset)
 		$indicator.addClass("visible");
 		$indicator.css("top", top + "px");
 	}
-
+	
+	this._UpdateTriggerLevel();
 	this._UpdateDisplay();
+}
+
+/*
+	The trigger level should stay the same until its steped
+	When changing the voltdiv the result should be clamped and displayed correctly, but when changing back, the level should change back as well.
+*/
+
+// Takes the unclamped value as basis, steps and sets a new value based on that.
+visir.AgilentOscilloscope.prototype._StepTriggerLevel = function(up)
+{
+	// side effect, if trigger mode is auto level, set to auto
+	if (this._triggerModeIdx == 0) this._SetTriggerMode(1);
+	
+	var level = this._ClampTriggerLevel(this._triggerLevelUnclamped);
+	
+	var trigch = this._channels[this._trigger.source - 1];
+	var stepsize = trigch.range / 10;
+	var newval = level + (up ? stepsize : -stepsize);
+	this._triggerLevelUnclamped = newval;
+	
+	this._SetTriggerLevel(newval, true);
+}
+
+// clamps the trigger level, sets up the serialized data and updates the display
+visir.AgilentOscilloscope.prototype._SetTriggerLevel = function(level, showLight)
+{
+	showLight = showLight || false;
+	level = this._ClampTriggerLevel(level);
+	
+	this._trigger.level = level;
+	
+	var $indicator = this._$elem.find(".triglevel .lighttext");
+	//this._$elem.find(".triglevel .lighttext").text(this._FormatValue(level) + "V");
+	this._LightIndicator($indicator);
+	this._UpdateTriggerLevel();
+}
+
+visir.AgilentOscilloscope.prototype._ClampTriggerLevel = function(level)
+{
+	var trigch = this._channels[this._trigger.source - 1];
+	var stepsize = trigch.range / 10;
+	level = Math.round(level / stepsize) * stepsize;
+	var max = -trigch.offset + (6*trigch.range);
+	var min = -trigch.offset - (6*trigch.range);
+	if (level > max) level = max;
+	if (level < min) level = min;
+	return level;
+}
+
+visir.AgilentOscilloscope.prototype._UpdateTriggerLevel = function()
+{
+	var trigch = this._channels[this._trigger.source - 1];
+	var level = this._ClampTriggerLevel(this._triggerLevelUnclamped);
+	this._trigger.level = level; // XXX: updating the serialized value here is not optimal..
+	
+	trace("update trigger level: " + (this._trigger.source - 1) + " " + level);
+
+	var markerPos = (((-level + trigch.offset) / trigch.range) ) * (this._plotHeight / 8.0) + (this._plotHeight / 2.0);
+	markerPos = Math.round(markerPos);
+	
+	var $group = this._$elem.find(".display .vertical .group.trigger_group");
+	$group.find(".ch_offset").removeClass("visible");
+	
+	if (markerPos <= 0) { // show overflow indicator
+		$group.find(".overflow_up").addClass("visible");
+	} else if (markerPos >= this._plotHeight) { // show underflow indicator
+		$group.find(".overflow_down").addClass("visible");
+	} else { // show normal indicator and move it into position
+		var $indicator = $group.find(".normal");
+		$indicator.addClass("visible");
+		$indicator.css("top", markerPos + "px");
+	}
+	
+	this._SetIndicatorValue(this._$elem.find(".triglevel .lighttext"), level);
+	//SetText(this._$elem.find(".triglevel .lighttext"), this._FormatValue(level));
+	//this._$elem.find(".triglevel .lighttext").text(this._FormatValue(level));
 }
 
 // XXX: maybe rename to ChButtonPressed or something..
@@ -454,6 +581,7 @@ visir.AgilentOscilloscope.prototype._SetTriggerSource = function(ch)
 {
 	this._trigger.source = ch;
 	this._$elem.find(".display .triggersource .channelname").text(ch);
+	this._UpdateTriggerLevel();
 }
 
 visir.AgilentOscilloscope.prototype._SetTriggerMode = function(modeIdx)
@@ -463,6 +591,7 @@ visir.AgilentOscilloscope.prototype._SetTriggerMode = function(modeIdx)
 	this._trigger.mode = this._triggerModes[modeIdx];
 	
 	this._$elem.find(".lighttext.triggermode").text(this._triggerModesLight[modeIdx]);
+	this._$elem.find(".menu_modecoupling .value.mode").text(this._triggerModesDisplay[this._triggerModeIdx]); // need to update the menu (if its active)
 }
 
 visir.AgilentOscilloscope.prototype._ShowMenu = function(menuname)
@@ -516,6 +645,20 @@ visir.AgilentOscilloscope.prototype._GetUnit = function(val)
 	return {unit: last[0], pow: last[1] };
 }
 
+// takes care of updating the light indicator value and sets the unit string correctly if present
+visir.AgilentOscilloscope.prototype._SetIndicatorValue = function($elem, value)
+{
+	var unit = this._GetUnit(value);
+	value /= Math.pow(10,unit.pow);
+	var str = value.toPrecision(3);
+
+	$elem.find(".strings").removeClass("visible");
+	$elem.find((unit.unit == "") ? ".large" : ".small").addClass("visible");
+	$elem.find(".top").text(unit.unit);
+	$elem.find(".value").text(this._FormatValue(str));
+	//SetText($elem, this._FormatValue(str));
+}
+
 visir.AgilentOscilloscope.prototype._LightIndicator = function($elem)
 {
 	if (this._activeIndicator && this._activeIndicator.Destroy) {
@@ -564,16 +707,81 @@ visir.AgilentOscilloscope.prototype._UpdateChannelDisplay = function(ch)
 
 visir.AgilentOscilloscope.prototype._UpdateDisplay = function()
 {
-		this._$elem.find(".voltage_ch1").text(this._FormatValue(this._voltages[this._voltIdx[0]]) + "V");
-		this._$elem.find(".voltage_ch2").text(this._FormatValue(this._voltages[this._voltIdx[1]]) + "V");
-		this._$elem.find(".timediv").text(this._FormatValue(this._timedivs[this._timeIdx]) + "s");
+	this._SetIndicatorValue(this._$elem.find(".voltage_ch1"), this._voltages[this._voltIdx[0]]);
+	this._SetIndicatorValue(this._$elem.find(".voltage_ch2"), this._voltages[this._voltIdx[1]]);
+	this._SetIndicatorValue(this._$elem.find(".timediv"), this._timedivs[this._timeIdx]);
+	//this._$elem.find(".voltage_ch1").text(this._FormatValue(this._voltages[this._voltIdx[0]]) + "V");
+	//this._$elem.find(".voltage_ch2").text(this._FormatValue(this._voltages[this._voltIdx[1]]) + "V");
+	//this._$elem.find(".timediv").text(this._FormatValue(this._timedivs[this._timeIdx]) + "s");
 						
-		this._DrawPlot(this._$elem.find(".plot"));
+	this._DrawPlot(this._$elem.find(".plot"));
 }
 
 visir.AgilentOscilloscope.prototype.ReadResponse = function(response) {
 	visir.AgilentOscilloscope.parent.ReadResponse.apply(this, arguments)
 	this._DrawPlot(this._$elem.find(".plot"));
+	
+	// check if we should continue measuring
+	if (this._isMeasuringContinuous && this._options.CheckToContinueCalling()) {
+		this._options.MeasureCalling();
+	} else {
+		this._isMeasuringContinuous = false;
+		this._$elem.find(".button.runstop .state").removeClass("visible");
+		this._$elem.find(".button.runstop .state.red").addClass("visible");
+		this._$elem.find(".button.single .state").removeClass("visible");
+		this._$elem.find(".button.single .state.dark").addClass("visible");
+	}
+}
+
+visir.AgilentOscilloscope.prototype._MakeMeasurement = function(button) {
+	var me = this;
+	/*function callback(status)
+	{
+		trace("callback: " + status);
+		switch(status)
+		{
+			case "continue":
+			break;
+			
+			default: // "stopped", "cancelled"
+				me._$elem.find(".button.runstop .state").removeClass("visible");
+				me._$elem.find(".button.runstop .state.red").addClass("visible");
+				me._$elem.find(".button.single .state").removeClass("visible");
+				me._$elem.find(".button.single .state.dark").addClass("visible");
+				
+				this._isMeasuring = false;
+			break;
+		}
+	}
+	*/
+	
+	switch(button) {
+		case "single":
+			this._$elem.find(".button.runstop .state").removeClass("visible");
+			this._$elem.find(".button.runstop .state.dark").addClass("visible");
+			this._$elem.find(".button.single .state").removeClass("visible");
+			this._$elem.find(".button.single .state.light").addClass("visible");
+			
+			this._isMeasuringContinuous = false;
+			this._options.MeasureCalling();
+			
+		break;
+		case "runstop":
+			this._$elem.find(".button.single .state").removeClass("visible");
+			this._$elem.find(".button.single .state.dark").addClass("visible");
+			this._$elem.find(".button.runstop .state").removeClass("visible");
+			this._$elem.find(".button.runstop .state.green").addClass("visible");
+		
+			if (this._isMeasuringContinuous) {
+				this._isMeasuringContinuous = false;
+				this._$elem.find(".button.runstop .state").removeClass("visible");
+				this._$elem.find(".button.runstop .state.red").addClass("visible");
+			} else {
+				this._isMeasuringContinuous = true;
+				this._options.MeasureCalling();
+			}
+		break;
+	}
 }
 
 function CreateChannelMenu(osc, ch, $menu)
@@ -647,8 +855,9 @@ function CreateTriggerModeCouplingMenu(osc, $menu)
 	var me = osc;
 	var timer = null;
 	return {
-		GetName: function() { return "Mode / Coupling Menu"; this.Redraw(); },
+		GetName: function() { this.Redraw(); return "Mode / Coupling Menu"; },
 		ButtonPressed: function(nr) {
+			this.Redraw();
 			switch(nr) {
 				case 1:
 					if (!this.ShowMenu("sel_trigger_mode")) return;
