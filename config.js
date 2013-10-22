5e51e297a6fcf2d50f23aee828f3f7ecd021a900
JohanZackrisson <johanz@gmail.com>
2013-10-22 11:19:13 +0200

    Add configuration file support

--- config.js
+++ config.js
@@ -4,6 +4,17 @@ visir.ConfigClass = function()
 {
 	this._teacherMode = true;
 	this._instrReg = null;
+	this._manualInstConfig = null;
+	
+	this._deferred = this.LoadConfig("config.xml")
+	this._isLoaded = false;
+}
+
+visir.ConfigClass.prototype.WaitUntilLoaded = function()
+{
+	// busy wait, not that good
+	// XXX: this could be solved in the visir.js loader
+	while(!this._isLoaded) {}
 }
 
 visir.ConfigClass.prototype.Get = function(name)
@@ -20,11 +31,39 @@ visir.ConfigClass.prototype.SetInstrRegistry = function(registry)
 	this._instrReg = registry;
 }
 
+visir.ConfigClass.prototype.SetManualInstrConfig = function(instrmap)
+{
+	this._manualInstConfig = instrmap;
+}
 
 visir.ConfigClass.prototype.GetNrInstrOfType = function(type)
 {
-	if (this._instrReg) return this._instrReg.GetNrInstrOfType(type);
+	this.WaitUntilLoaded();
+	if (this._manualInstConfig) return this._manualInstConfig[type];
+	if (this._instrReg) return this._instrReg.GetNrInstrOfType(type);	
 	return 1;
+}
+
+// XXX: There is a timing problem with this, properties can be read before the config is read
+visir.ConfigClass.prototype.LoadConfig = function(url)
+{
+	var me = this;
+	return $.ajax({
+		type: "GET",
+		url: url,
+		dataType: "xml",
+		/*async: true, //can't use async: true, chrome bugs out */
+		cache: false
+	}).success( function(rawxml) {
+		var $xml = $(rawxml);
+		var teacher = parseInt($xml.find("teacher").text(), 10);
+		me._teacherMode = teacher | false;
+	})
+	.fail(function() { trace("config.xml not found"); })
+	.always(function() {
+		me._isLoaded = true;
+	})
+	;
 }
 
 visir.Config = new visir.ConfigClass();
